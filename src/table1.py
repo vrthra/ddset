3881b338fe69f80f2d2936c6f4133bc09a489489
Rahul Gopinath <rahul@gopinath.org>
2020-04-24 12:20:26 +0200

    update

--- src/table1.py
+++ src/table1.py
@@ -115,6 +115,7 @@ def table1_row(j, fname):
 def table1(args):
     rows = []
     for a in args:
+        if '.log.' in a: continue
         with open(a) as f:
             j = json.load(fp=f)
         r = table1_row(j, a)
